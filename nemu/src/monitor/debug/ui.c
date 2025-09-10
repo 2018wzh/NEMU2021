bacd5bc67b1b47ba658074f840ecb7faee950ad6
2018wzh <wzh20070720@outlook.com>
2025-09-10 23:00:09 +0800

    PA1-Stage1-Task2

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -1,111 +1,175 @@
-#include "monitor/monitor.h"
 #include "monitor/expr.h"
+#include "monitor/monitor.h"
 #include "monitor/watchpoint.h"
 #include "nemu.h"
 
-#include <stdlib.h>
-#include <readline/readline.h>
 #include <readline/history.h>
+#include <readline/readline.h>
+#include <stdlib.h>
 
 void cpu_exec(uint32_t);
 
-/* We use the `readline' library to provide more flexibility to read from stdin. */
-char* rl_gets() {
-	static char *line_read = NULL;
+/* We use the `readline' library to provide more flexibility to read from stdin.
+ */
+char *rl_gets() {
+  static char *line_read = NULL;
 
-	if (line_read) {
-		free(line_read);
-		line_read = NULL;
-	}
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
 
-	line_read = readline("(nemu) ");
+  line_read = readline("(nemu) ");
 
-	if (line_read && *line_read) {
-		add_history(line_read);
-	}
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
 
-	return line_read;
+  return line_read;
 }
 
-static int cmd_c(char *args) {
-	cpu_exec(-1);
-	return 0;
-}
+static int cmd_c(char *args);
 
-static int cmd_q(char *args) {
-	return -1;
-}
+static int cmd_q(char *args);
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
+static int cmd_info(char *args);
+
+static int cmd_x(char *args);
+
 static struct {
-	char *name;
-	char *description;
-	int (*handler) (char *);
-} cmd_table [] = {
-	{ "help", "Display informations about all supported commands", cmd_help },
-	{ "c", "Continue the execution of the program", cmd_c },
-	{ "q", "Exit NEMU", cmd_q },
-
-	/* TODO: Add more commands */
-
+  char *name;
+  char *description;
+  int (*handler)(char *);
+} cmd_table[] = {
+    {"help", "Display informations about all supported commands", cmd_help},
+    {"c", "Continue the execution of the program", cmd_c},
+    {"q", "Exit NEMU", cmd_q},
+    /* TODO: Add more commands */
+    {"si", "Step through the program for N instructions", cmd_si},
+    {"info", "Print register state or watchpoint state", cmd_info},
+    {"x", "Scan memory", cmd_x},
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
 static int cmd_help(char *args) {
-	/* extract the first argument */
-	char *arg = strtok(NULL, " ");
-	int i;
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
 
-	if(arg == NULL) {
-		/* no argument given */
-		for(i = 0; i < NR_CMD; i ++) {
-			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-		}
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  } else {
+    for (i = 0; i < NR_CMD; i++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+static int cmd_c(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
+static int cmd_q(char *args) { return -1; }
+
+static int cmd_si(char *args) {
+  int N;
+  if (args == NULL)
+    N = 1;
+  else
+    N = atoi(args);
+  if (N <= 0) {
+    printf("Invalid arguments: %s\n", args);
+    return 0;
+  }
+  cpu_exec(N);
+  return 0;
+}
+
+static int cmd_info(char *args) {
+  if (strcmp(args, "r") == 0) {
+    int i;
+    for (i = R_EAX; i <= R_EDI; i++) {
+      printf("%s\t0x%08x\t%d\n", regsl[i], reg_l(i), reg_l(i));
+    }
+    printf("eip\t0x%08x\t%d\n", cpu.eip, cpu.eip);
+    printf("eflags\t0x%08x\n", cpu.eflags.val);
+    return 0;
+  } else {
+    printf("Invalid arguments: '%s'\n", args);
+    return 0;
+  }
+}
+
+static int cmd_x(char *args) {
+  int n;
+  swaddr_t addr;
+  int result = sscanf(args, "%d %x", &n, &addr);
+  if (result != 2 || n <= 0 || addr < 0) {
+    printf("Invalid arguments: %s\n", args);
+    return 0;
+  }
+  for (int i = 0; i < n; i++) {
+	if (i % 4 == 0) {
+	  printf("0x%08x: ", addr + i * 4);
 	}
-	else {
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(arg, cmd_table[i].name) == 0) {
-				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-				return 0;
-			}
-		}
-		printf("Unknown command '%s'\n", arg);
+	printf("0x%08x ", swaddr_read(addr + i * 4, 4));
+	if (i % 4 == 3 || i == n - 1) {
+	  printf("\n");
 	}
-	return 0;
+  }
+  return 0;
 }
 
 void ui_mainloop() {
-	while(1) {
-		char *str = rl_gets();
-		char *str_end = str + strlen(str);
+  while (1) {
+    char *str = rl_gets();
+    char *str_end = str + strlen(str);
 
-		/* extract the first token as the command */
-		char *cmd = strtok(str, " ");
-		if(cmd == NULL) { continue; }
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) {
+      continue;
+    }
 
-		/* treat the remaining string as the arguments,
-		 * which may need further parsing
-		 */
-		char *args = cmd + strlen(cmd) + 1;
-		if(args >= str_end) {
-			args = NULL;
-		}
+    /* treat the remaining string as the arguments,
+     * which may need further parsing
+     */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
 
 #ifdef HAS_DEVICE
-		extern void sdl_clear_event_queue(void);
-		sdl_clear_event_queue();
+    extern void sdl_clear_event_queue(void);
+    sdl_clear_event_queue();
 #endif
 
-		int i;
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(cmd, cmd_table[i].name) == 0) {
-				if(cmd_table[i].handler(args) < 0) { return; }
-				break;
-			}
-		}
+    int i;
+    for (i = 0; i < NR_CMD; i++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) {
+          return;
+        }
+        break;
+      }
+    }
 
-		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-	}
+    if (i == NR_CMD) {
+      printf("Unknown command '%s'\n", cmd);
+    }
+  }
 }
